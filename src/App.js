734d4d571629356b1e235af478de0731dba4ee5c
mabenunez <39781996+mabenunez@users.noreply.github.com>
2019-10-24 10:59:56 -0300

    router uses a dinamic route & props.match.params

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import './App.css';
 import Homepage from './pages/homepage/Homepage.component';
-import { Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 const HatsPage = () => {
   return(
     <div>Hats Page</div>
   )
 }
-
+const TopicPage = (props) => {
+  console.log(props)
+  return(
+    <div>Topic Page {props.match.params.topicId}</div>
+  )
+}
 function App() {
   return (
     <div>
-      <Route path='/' component={Homepage}></Route>
-      <Route path='/hats' component={HatsPage}></Route>
+      {/* Switch will only render the 1st path it encounters that matches */}
+      <Switch> 
+        <Route exact path='/' component={Homepage}></Route>
+        <Route exact path='/hats' component={HatsPage}></Route>
+        <Route exact path='/topics/:topicId' component={TopicPage}></Route>
+      </Switch>
     </div>
   );
 }
